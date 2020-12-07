d3acdab58b9dcb3c6773ae50526b31c17d2c90bf
sn4rkie <72408470+sn4rkie@users.noreply.github.com>
2020-12-07 15:06:47 -0600

    updating weather api and forms

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function displayForecast(response) {
 	iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 	iconElement.setAttribute("alt", `${response.data.weather[0].description}`)
 
-	function convertToFahrenheit () {
+	function convertToFahrenheit() {
 		let fahrenheit = Math.round(response.data.main.temp * (9 / 5) + 32);	
 		document.querySelector(`.temperature`).innerHTML = `${fahrenheit}°F`;
 	}
@@ -29,7 +29,7 @@ function displayForecast(response) {
 
 function showPosition(position) {
 	let apiKey = "4b71899235f734ae8df5f2e02f0aba1a";
-	let apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?`;
+	let apiEndpoint = `https://api.openweathermap.org/data/2.5/forecast?`;
 	let localApiUrl = `${apiEndpoint}lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
 	axios.get(localApiUrl).then(displayForecast);
 }
@@ -44,30 +44,30 @@ function formatToday() {
 	let date = new Date();
 	let weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	let weekday = weekdays[date.getDay()];
-	let months = ["January", 
-	"February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	let month = months[date.getMonth()];
-	document.querySelector(`.date`).innerHTML = 
-	(`${weekday}, ${month} ${date.getDate()}, ${date.getFullYear()}`);
-
 	let h = date.getHours();
 	let m = date.getMinutes();
 	if (h < 10) {h = `0${h}`;}
 	if (m < 10) {m = `0${m}`;}
+
+	document.querySelector(`.date`).innerHTML = (`${weekday}, ${month} ${date.getDate()}, ${date.getFullYear()}`);
     document.querySelector(".clock").innerHTML = h + ":" + m;
     
     setTimeout(formatToday, 1000);  
 }
 formatToday();
 
-
 function handleSearch(event) {
-	let city = prompt(`What city would you like the weather for?`);
 	event.preventDefault();
-	search(city);
+	let city= document.querySelector(`#cityInput`);
+	search(city.value);
 }
 let searchButton = document.querySelector(`.searchButton`);
 searchButton.addEventListener("click", handleSearch);
+
+let form = document.querySelector(`#search-form`);
+form.addEventListener("submit", handleSearch)
 
 function search(city) {
 	let apiKey = "4b71899235f734ae8df5f2e02f0aba1a";
@@ -75,11 +75,7 @@ function search(city) {
 	let searchApiUrl = `${apiEndpoint}q=${city}&units=metric&appid=${apiKey}`;
 	axios.get(searchApiUrl).then(displayForecast);
 }
-
-
 search(`Paradise City`);
-
-
 
 
 
